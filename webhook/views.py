d51c6bb220eb395b0d88bc34ffd8b4499bb08b95
brumel <brumel.serga@gmail.com>
2017-06-12 21:46:39 +0300

    test getME

--- webhook/views.py
+++ webhook/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from bot import settings
+import telepot
+
+TelegramBot = telepot.Bot(settings.BOT_TOCKEN)
 
 def index(request):
-    return HttpResponse("Hello, world!")
+
+    return HttpResponse(TelegramBot.getMe())
