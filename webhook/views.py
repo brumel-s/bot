15d0e3952ba1cb34ea05cb62cf59a7ae3e09f196
brumel <brumel.serga@gmail.com>
2017-06-12 22:34:47 +0300

    d=first message

--- webhook/views.py
+++ webhook/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from bot import settings
 import telepot
+import pprint
 
 TelegramBot = telepot.Bot(settings.BOT_TOCKEN)
 
 def index(request):
 
-    return HttpResponse(TelegramBot.getMe())
+    updates = TelegramBot.getUpdates();
+    for update in updates:
+        TelegramBot.sendMessage(update['message']['chat']['id'], "You said something")
+        return HttpResponse(pprint.pformat(update['message']['chat']['id'], indent=4))
+    #return HttpResponse(me['username'])
