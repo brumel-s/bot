9adf7b20d10b4f80cec4617ea5405a434054165b
brumel <brumel.serga@gmail.com>
2017-06-12 22:48:46 +0300

    gitignore

--- webhook/views.py
+++ webhook/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from bot import settings
 import telepot
+import urllib3
 import pprint
+
+if settings.IS_PYTHONANYWERE:
+    proxy_url = "http://proxy.server:3128"
+    telepot.api._pools = {
+        'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+    }
+    telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
 TelegramBot = telepot.Bot(settings.BOT_TOCKEN)
 
