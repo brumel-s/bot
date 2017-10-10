0356d88aa64fbd695f1a69583b8a79cff5b91b79
Serhii Belmas <sbelmas@unisender.com>
2017-10-10 16:31:56 +0300

    fix message is to long for campaigns

--- api/api_commands/campaigns.py
+++ api/api_commands/campaigns.py
@@ -67,7 +67,7 @@ class Campaigns(BaseCommand):
             except ValueError:
                 raise CmdException("Неверный формат даты \"От\"")
 
-        return {"from": date_from, "to": date_to, "limit": 30}
+        return {"from": date_from, "to": date_to, "limit": 15}
 
 
     def prepare_telegram_response(self, response):
